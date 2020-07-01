f55b275dd4ff32c1ad71cb580677ddf91974f388
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-07-01 14:01:07 -0400

    Accepting 90% accuracy; some randomness in results

---
+++ example/test.py
@@ -0,0 +1,17 @@
+def test(file1, file2):
+   firstfile = open(file1, 'r')
+   secondfile = open(file2, 'r')
+   lines1 = set()
+   lines2 = set()
+   for line in firstfile:
+      lines1.add(line)
+   for line in secondfile:
+      lines2.add(line)
+
+   comb = lines1.intersection(lines2)
+
+   # Some random number inconsistency; we will accept 90% accuracy
+   if (len(comb) >= 0.9*len(lines2)):
+      return True
+   else:
+      return False
